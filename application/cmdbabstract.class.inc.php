ucbpi/itop

commit 960864bcbb39aaf4e6831d014b84da0326e750ed

#1000 Implemented the behavior for the flag OPT_ATT_MUSTCHANGE, and took the opportunity to add a feedback when a field is mandatory OR when the format is wrong
git-svn-id: http://svn.combodo.net/subversion/itop.sf.net/trunk@3394 60fe80c7-c1f8-43be-bd32-c3e8feb7a3b0

application/cmdbabstract.class.inc.php
@@ -1854,7 +1854,8 @@ abstract class cmdbAbstractObject extends CMDBObject implements iDisplay
 				{
 					$sNullValue = "'$sNullValue'"; // Add quotes to turn this into a JS string if it's not a number
 				}
-				$oPage->add_ready_script("$('#$iId').bind('".implode(' ', $aEventsList)."', function(evt, sFormId) { return ValidateField('$iId', '$sPattern', $bMandatory, sFormId, $sNullValue) } );\n"); // Bind to a custom event: validate
+				$sOriginalValue = ($iFlags & OPT_ATT_MUSTCHANGE) ? "'".addslashes($value)."'" : 'undefined';
+				$oPage->add_ready_script("$('#$iId').bind('".implode(' ', $aEventsList)."', function(evt, sFormId) { return ValidateField('$iId', '$sPattern', $bMandatory, sFormId, $sNullValue, $sOriginalValue) } );\n"); // Bind to a custom event: validate
 			}
 			$aDependencies = MetaModel::GetDependentAttributes($sClass, $sAttCode); // List of attributes that depend on the current one
 			if (count($aDependencies) > 0)
@@ -1863,6 +1864,9 @@ abstract class cmdbAbstractObject extends CMDBObject implements iDisplay
 				$oPage->add_ready_script("$('#$iId').unbind('change.dependencies').bind('change.dependencies', function(evt, sFormId) { return oWizardHelper{$sFormPrefix}.UpdateDependentFields(['".implode("','", $aDependencies)."']) } );\n"); // Bind to a custom event: validate
 			}
 		}
+		$oPage->add_dict_entry('UI:ValueMustBeSet');
+		$oPage->add_dict_entry('UI:ValueMustBeChanged');
+		$oPage->add_dict_entry('UI:ValueInvalidFormat');
 		return "<div>{$sHTMLValue}</div>";
 	}
 

css/light-grey.css
@@ -1416,3 +1416,7 @@ div.ui-dialog-header {
     padding-bottom: 10px;
     padding-top: 7px;
 }
+.form_field_error {
+	border: 1px solid #933;
+	background: #fcc;
+}

datamodels/1.x/itop-change-mgmt-1.0.0/datamodel.itop-change-mgmt.xml
@@ -457,7 +457,7 @@
                 <hidden/>
               </attribute>
               <attribute id="agent_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>
@@ -466,13 +466,13 @@
                 <mandatory/>
               </attribute>
               <attribute id="supervisor_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="manager_group_id">
                 <mandatory/>
               </attribute>
               <attribute id="manager_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="outage">
                 <hidden/>
@@ -1488,7 +1488,7 @@
                 <hidden/>
               </attribute>
               <attribute id="agent_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>
@@ -1497,13 +1497,13 @@
                 <mandatory/>
               </attribute>
               <attribute id="supervisor_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="manager_group_id">
                 <mandatory/>
               </attribute>
               <attribute id="manager_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="outage">
                 <hidden/>
@@ -2465,7 +2465,7 @@
                 <hidden/>
               </attribute>
               <attribute id="agent_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>
@@ -2474,13 +2474,13 @@
                 <mandatory/>
               </attribute>
               <attribute id="supervisor_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="manager_group_id">
                 <mandatory/>
               </attribute>
               <attribute id="manager_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="outage">
                 <hidden/>
@@ -3471,7 +3471,7 @@
                 <hidden/>
               </attribute>
               <attribute id="agent_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>
@@ -3480,13 +3480,13 @@
                 <mandatory/>
               </attribute>
               <attribute id="supervisor_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="manager_group_id">
                 <mandatory/>
               </attribute>
               <attribute id="manager_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="outage">
                 <hidden/>
@@ -4547,7 +4547,7 @@
                 <hidden/>
               </attribute>
               <attribute id="agent_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>
@@ -4556,13 +4556,13 @@
                 <mandatory/>
               </attribute>
               <attribute id="supervisor_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="manager_group_id">
                 <mandatory/>
               </attribute>
               <attribute id="manager_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="outage">
                 <hidden/>

datamodels/1.x/itop-incident-mgmt-1.0.0/datamodel.itop-incident-mgmt.xml
@@ -54,7 +54,7 @@
                 <read_only/>
               </attribute>
               <attribute id="description">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="start_date">
                 <read_only/>
@@ -63,28 +63,28 @@
                 <mandatory/>
               </attribute>
               <attribute id="org_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="service_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="servicesubcategory_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="product">
                 <must_prompt/>
               </attribute>
               <attribute id="impact">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="urgency">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="priority">
                 <read_only/>
               </attribute>
               <attribute id="workgroup_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>

datamodels/1.x/itop-problem-mgmt-1.0.0/datamodel.itop-problem-mgmt.xml
@@ -213,7 +213,7 @@
                 <read_only/>
               </attribute>
               <attribute id="description">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="ticket_log">
                 <hidden/>
@@ -222,28 +222,28 @@
                 <read_only/>
               </attribute>
               <attribute id="org_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="service_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="servicesubcategory_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="product">
                 <must_prompt/>
               </attribute>
               <attribute id="impact">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="urgency">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="priority">
                 <read_only/>
               </attribute>
               <attribute id="workgroup_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_id">
                 <hidden/>

datamodels/1.x/itop-request-mgmt-1.0.0/datamodel.itop-request-mgmt.xml
@@ -91,7 +91,7 @@
                 <read_only/>
               </attribute>
               <attribute id="description">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="start_date">
                 <read_only/>
@@ -100,28 +100,28 @@
                 <mandatory/>
               </attribute>
               <attribute id="org_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="service_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="servicesubcategory_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="product">
                 <must_prompt/>
               </attribute>
               <attribute id="impact">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="urgency">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="priority">
                 <read_only/>
               </attribute>
               <attribute id="workgroup_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>

datamodels/1.x/itop-tickets-1.0.0/datamodel.itop-tickets.xml
@@ -422,7 +422,7 @@
                 <read_only/>
               </attribute>
               <attribute id="description">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="start_date">
                 <read_only/>
@@ -431,28 +431,28 @@
                 <mandatory/>
               </attribute>
               <attribute id="org_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="service_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="servicesubcategory_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="product">
                 <must_prompt/>
               </attribute>
               <attribute id="impact">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="urgency">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="priority">
                 <read_only/>
               </attribute>
               <attribute id="workgroup_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="agent_email">
                 <hidden/>

datamodels/2.x/itop-problem-mgmt/datamodel.itop-problem-mgmt.xml
@@ -169,7 +169,7 @@
                 <read_only/>
               </attribute>
               <attribute id="org_id">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="team_id">
                 <hidden/>
@@ -178,7 +178,7 @@
                 <hidden/>
               </attribute>
               <attribute id="description">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="start_date">
                 <read_only/>
@@ -199,10 +199,10 @@
                 <normal/>
               </attribute>
               <attribute id="impact">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="urgency">
-                <must_change/>
+                <must_prompt/>
               </attribute>
               <attribute id="priority">
                 <read_only/>

commit 0717b4eb4d06b0e9d4ce494105abd1dc10d90b40

Fix for a regression introduced by [r3394] while fixing #1000: handling of OPT_ATT_MUSTCHANGE. The fix was not working when the original value contained several lines.

application/cmdbabstract.class.inc.php
@@ -1854,7 +1854,7 @@ abstract class cmdbAbstractObject extends CMDBObject implements iDisplay
 				{
 					$sNullValue = "'$sNullValue'"; // Add quotes to turn this into a JS string if it's not a number
 				}
-				$sOriginalValue = ($iFlags & OPT_ATT_MUSTCHANGE) ? "'".addslashes($value)."'" : 'undefined';
+				$sOriginalValue = ($iFlags & OPT_ATT_MUSTCHANGE) ? json_encode($value) : 'undefined';
 				$oPage->add_ready_script("$('#$iId').bind('".implode(' ', $aEventsList)."', function(evt, sFormId) { return ValidateField('$iId', '$sPattern', $bMandatory, sFormId, $sNullValue, $sOriginalValue) } );\n"); // Bind to a custom event: validate
 			}
 			$aDependencies = MetaModel::GetDependentAttributes($sClass, $sAttCode); // List of attributes that depend on the current one